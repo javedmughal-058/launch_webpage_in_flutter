7c069073c6639d85b339822ee560dc379ad76752
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-17 14:24:15 +0500

    Version update available notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:upgrader/upgrader.dart';
+import 'package:url_launcher/link.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -37,14 +39,54 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Web Launch')),
-        body: Center(
-          child: ElevatedButton(
-              onPressed: (){
-                 String _url = 'https://africaexchange.cash/admin/cnetpay/cnetpay.php?user_id=25&receiver_country=Ivory-XOF&amount=300&fee=100&total_pay=400&receiver_f_name=Sarem&receiver_l_name=Iftikhar&receiver_email=sarimiftikhar@gmail.com&receiver_phone=03361149673&currency_type=XAF&status=0&transaction_id=29797053';
-                openBrowserUrl(url : _url, inApp: false);
+        body: UpgradeAlert(
+          upgrader: Upgrader(
+            dialogStyle: UpgradeDialogStyle.material
+          ),
+          child: Center(
+            child: Container(
+              margin: EdgeInsets.all(40),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            // maximumSize: MediaQuery.of(context).size.width,
+                            primary: Colors.white, //background color of button
+                            side: BorderSide(width:3, color:Colors.green), //border width and color
+                            elevation: 3, //elevation of button
+                            padding: EdgeInsets.all(20) //content padding inside button
+                        ),
+                        onPressed: (){
+                           String _url = 'https://africaexchange.cash/admin/cnetpay/cnetpay.php?user_id=25&receiver_country=Ivory-XOF&amount=300&fee=100&total_pay=400&receiver_f_name=Sarem&receiver_l_name=Iftikhar&receiver_email=sarimiftikhar@gmail.com&receiver_phone=03361149673&currency_type=XAF&status=0&transaction_id=29797053';
+                          openBrowserUrl(url : _url, inApp: false);
 
-              },
-              child: const Text('View')),
+                        },
+                        child: const Text('View')),
+                  ),
+                  Link(
+                    target: LinkTarget.self,
+                    uri: Uri.parse('https://africaexchange.cash/admin/cnetpay/cnetpay.php?user_id=25&receiver_country=Ivory-XOF&amount=300&fee=100&total_pay=400&receiver_f_name=Sarem&receiver_l_name=Iftikhar&receiver_email=sarimiftikhar@gmail.com&receiver_phone=03361149673&currency_type=XAF&status=0&transaction_id=29797053'),
+                    builder: (context, followLink)=> ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          // maximumSize: MediaQuery.of(context).size.width,
+                            primary: Colors.white, //background color of button
+                            side: BorderSide(width:3, color:Colors.green), //border width and color
+                            elevation: 3, //elevation of button
+                            padding: EdgeInsets.all(20) //content padding inside button
+                        ),
+                        onPressed: followLink,
+                          // String _url = 'https://africaexchange.cash/admin/cnetpay/cnetpay.php?user_id=25&receiver_country=Ivory-XOF&amount=300&fee=100&total_pay=400&receiver_f_name=Sarem&receiver_l_name=Iftikhar&receiver_email=sarimiftikhar@gmail.com&receiver_phone=03361149673&currency_type=XAF&status=0&transaction_id=29797053';
+                          // openBrowserUrl(url : _url, inApp: false);
+
+
+                        child: const Text('View')),
+                  ),
+                ],
+              ),
+            ),
+          ),
         )
     );
   }
